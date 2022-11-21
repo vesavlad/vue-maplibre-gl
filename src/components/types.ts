c75031b14c824238e68a920dc8445bd318ae34fa
Vlad Vesa <vlad.vesa@outlook.com>
2022-11-21 10:08:17 +0200

    mapSymbol expects shallowRef

--- src/components/types.ts
+++ src/components/types.ts
@@ -1,10 +1,10 @@
-import { InjectionKey, Ref } from 'vue';
+import { InjectionKey, Ref, ShallowRef } from 'vue';
 import { Map } from 'maplibre-gl';
 import { MglMap } from '@/components/index';
 import { Emitter } from 'mitt';
 import { SourceLayerRegistry } from '@/components/sources/sourceLayer.registry';
 
-export const mapSymbol: InjectionKey<Ref<Map>> = Symbol('map'),
+export const mapSymbol: InjectionKey<ShallowRef<Map>> = Symbol('map'),
 	isLoadedSymbol: InjectionKey<Ref<boolean>> = Symbol('isLoaded'),
 	componentIdSymbol: InjectionKey<number> = Symbol('componentId'),
 	sourceIdSymbol: InjectionKey<string> = Symbol('sourceId'),
