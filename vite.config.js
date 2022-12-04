1d476a695335154c31398a22a6b0e8615ef0c8f7
Vlad Vesa <vlad.vesa@outlook.com>
2022-12-04 11:08:18 +0200

    change packaging rules

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,15 @@ import { resolve } from 'path';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import dts from 'vite-plugin-dts';
+import pkg from './package.json';
 
+export const banner = `/*!
+* ${pkg.name} v${pkg.version}
+* (c) ${new Date().getFullYear()} ${pkg.author.name}
+* @license ${pkg.license}
+*/`;
+
+/** @type {import('vite').UserConfig} */
 export default defineConfig({
 	plugins: [vue(), dts({ outputDir: 'dist/types' })],
 	resolve: {
@@ -17,21 +25,37 @@ export default defineConfig({
 			entry: resolve(__dirname, 'src/entry.ts'),
 			name: 'VueMaplibreGl',
 			fileName: (format) => {
-				let desc = { cjs: 'ssr', es: 'esm', iife: 'min' }[format] || format;
+				let desc = { cjs: 'ssr', es: 'esm', umd: 'min' }[format] || format;
 				return 'vue-maplibre-gl.' + desc + '.js';
 			},
-			formats: ['cjs', 'es', 'iife'],
 		},
-		sourcemap: true,
 		rollupOptions: {
 			external: ['vue', 'maplibre-gl'],
-			output: {
-				exports: 'named',
-				globals: {
-					vue: 'Vue',
-					'maplibre-gl': 'maplibregl',
+			output: [
+				{
+					format: 'cjs',
+					exports: 'named',
+					strict: true,
+					banner: banner,
 				},
-			},
+				{
+					format: 'es',
+					exports: 'named',
+					sourcemap: true,
+					banner: banner,
+				},
+				{
+					format: 'umd',
+					exports: 'named',
+					banner: banner,
+					sourcemap: true,
+					globals: {
+						vue: 'vue',
+						mitt: 'mitt',
+						'maplibre-gl': 'maplibregl',
+					},
+				},
+			],
 		},
 	},
 });
